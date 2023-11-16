4c387b7934f4675b9be589c1e34ba19fc2d2a384
beatrixdaa <beatrixdevanti21@gmail.com>
2023-11-16 14:07:33 +0700

    revisi main activity

--- app/src/main/java/com/example/tugas5_esjumbo/MainActivity.kt
+++ app/src/main/java/com/example/tugas5_esjumbo/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.tugas5_esjumbo.ui.HalamanDua
 import com.example.tugas5_esjumbo.ui.HalamanHome
+import com.example.tugas5_esjumbo.ui.HalamanSatu
 import com.example.tugas5_esjumbo.ui.theme.Tugas5_esjumboTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +25,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HalamanHome{}
+                    HalamanHome {
+
+                    }
                 }
             }
         }
@@ -34,6 +38,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     Tugas5_esjumboTheme {
-        HalamanHome{}
+        HalamanHome {
+
+        }
     }
 }
