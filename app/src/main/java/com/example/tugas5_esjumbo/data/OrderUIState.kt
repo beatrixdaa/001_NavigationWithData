512df56ff79a32b329977eec46961dae7e1a4268
beatrixdaa <beatrixdevanti21@gmail.com>
2023-11-16 13:14:42 +0700

    add var nama,alamat,tlp

--- app/src/main/java/com/example/tugas5_esjumbo/data/OrderUIState.kt
+++ app/src/main/java/com/example/tugas5_esjumbo/data/OrderUIState.kt
@@ -4,4 +4,7 @@ data class OrderUIState(
     val jumlah: Int = 0,
     val rasa: String = "",
     val harga: String = "",
+    var nama:String="",
+    var alamat:String="",
+    var tlp:String=""
 )
